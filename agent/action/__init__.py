5bd514cd0ab739fa0dfc2c5b166195b69c2a6c97
SwordofMorning <969211735@qq.com>
2025-12-07 21:56:26 +0800

    CI: 删除Borderless

--- agent/action/__init__.py
+++ agent/action/__init__.py
@@ -3,9 +3,6 @@ Action package initialization
 Provides unified access to all action module functionalities
 """
 
-# Import submodules
-from . import borderless
-
 # Import log module
 from . import log
 
@@ -18,21 +15,14 @@ from .log import (
     Enable_MaaLog_Info
 )
 
-# Import borderless functions
-from .borderless import get_global_optimizer
-
 # Define what gets exported when using "from action import *"
 __all__ = [
     # Submodules
     'log', 
-    'borderless',
     
     # Logging functions
     'MaaLog_Debug',
     'MaaLog_Info',
-    
-    # Borderless functions
-    'get_global_optimizer',
     
     # Global variables
     'Task_Counter',
@@ -43,4 +33,4 @@ __all__ = [
 # Package metadata
 __version__ = '1.0.1'
 __author__ = 'MaaGF1 Team'
-__description__ = 'Action module for MaaFramework agent (Refactored)'
+__description__ = 'Action module for MaaFramework agent'
